a372e904c2e5e179872e7f49ae25a20829887959
Jan-Eric Waclawski <memorix101@gmail.com>
2017-10-03 01:41:37 +0200

    Added missing repo

---
+++ MonoGame_SpaceInvaders-master/Game/Explosion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Diagnostics;
+
+namespace UWP_SpaceInvaders
+{
+    class Explosion
+    {
+        private Vector2 pos = new Vector2(0, 0);
+        private Texture2D tex;
+        private float PosX, PosY;
+        private const int moveSpeed = 50;
+
+        private float startPos;
+
+        private Rectangle spriteRect = new Rectangle(0, 0, 128, 128);
+
+        private const float animationSpeed = 25;
+        private float currentFrame;
+
+        float spriteWidth;
+
+        public bool animationCompleted = false;
+
+        public Explosion(Vector2 position, ContentManager con)
+        {
+            startPos = position.X;
+            PosX = position.X;
+            PosY = position.Y;
+            tex = con.Load<Texture2D>("explode");
+            spriteWidth = tex.Bounds.Width;
+        }
+
+        public Vector2 Position
+        {
+            get { return pos; }
+        }
+
+        private void Animator(GameTime gameTime)
+        {
+            currentFrame += animationSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            currentFrame = MathHelper.Clamp(currentFrame, 0, 16);
+
+            if (currentFrame >= 16)
+            {
+                //currentFrame = 0;
+                animationCompleted = true;
+            }
+
+            spriteRect = new Rectangle((int)currentFrame * 128, 0, 128, 128);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Animator(gameTime);
+
+            pos = new Vector2(PosX, PosY);
+        }
+
+        public void Draw(SpriteBatch sp)
+        {
+            sp.Draw(tex, pos, spriteRect, Color.White);
+        }
+    }
+}
---
+++ MonoGame_SpaceInvaders-master/README.md
@@ -0,0 +1,7 @@
+# MonoGame_SpaceInvaders
+
+UWP MonoGame App for Windows 10 Anniversary and Xbox One
+
+![screenshot 13](https://cloud.githubusercontent.com/assets/1466920/20637134/2a66e3ae-b37d-11e6-9181-ae7c45695b75.png)
+
+https://www.youtube.com/watch?v=cywd2-lcHms&feature=youtu.be
