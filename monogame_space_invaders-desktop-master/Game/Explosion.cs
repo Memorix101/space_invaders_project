e9684e9032ae6a12bf13dd2762b8262470f377c1
Jan-Eric Waclawski <memorix101@gmail.com>
2019-04-12 00:13:24 +0200

    Updated MonoGame DesktopGL version

--- monogame_space_invaders-desktop-master/Game/Explosion.cs
+++ monogame_space_invaders-desktop-master/Game/Explosion.cs
@@ -12,16 +12,10 @@ namespace SpaceInvaders_Desktop
         private Texture2D tex;
         private float PosX, PosY;
         private const int moveSpeed = 50;
-
         private float startPos;
-
         private Rectangle spriteRect = new Rectangle(0, 0, 128, 128);
-
         private const float animationSpeed = 25;
         private float currentFrame;
-
-        float spriteWidth;
-
         public bool animationCompleted = false;
 
         public Explosion(Vector2 position, ContentManager con)
@@ -30,7 +24,6 @@ namespace SpaceInvaders_Desktop
             PosX = position.X;
             PosY = position.Y;
             tex = con.Load<Texture2D>("explode");
-            spriteWidth = tex.Bounds.Width;
         }
 
         public Vector2 Position
@@ -41,12 +34,10 @@ namespace SpaceInvaders_Desktop
         private void Animator(GameTime gameTime)
         {
             currentFrame += animationSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
             currentFrame = MathHelper.Clamp(currentFrame, 0, 16);
 
             if (currentFrame >= 16)
             {
-                //currentFrame = 0;
                 animationCompleted = true;
             }
 
