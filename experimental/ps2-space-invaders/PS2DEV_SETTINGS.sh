c906f407d88e509a1b638f70cc07633b4e4ca47f
Memorix101 <memorix101@gmail.com>
2021-02-05 03:02:29 +0100

    ...

--- experimental/ps2-space-invaders/PS2DEV_SETTINGS.sh
+++
@@ -1,6 +0,0 @@
-## PS2DEV SETTINGS
-echo "INFO: run me as: source script.sh"
-export PS2DEV="/usr/local/ps2dev"
-export PS2SDK="$PS2DEV/ps2sdk"
-export PATH="$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin"
-export PATH="$PATH:$PS2DEV/dvp/bin:$PS2SDK/bin"
